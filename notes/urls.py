fb519d2f2d44a9dd2ca6aa74701a73fe71523c05
VeraAquarius <15821886267@163.com>
2025-03-29 20:45:50 +0800

    8 day

--- notes/urls.py
+++ notes/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('welcome/', views.welcome, name='welcome'),
     path('create/', views.create_note, name='create'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -6,6 +6,9 @@ from .models import Note
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import NoteForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 def welcome(request):
     return HttpResponse("欢迎来到我的笔记应用！")
@@ -23,3 +26,33 @@ def create_note(request):
     else:
         form = NoteForm()
     return render(request, 'notes/create_note.html', {'form': form})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'notes/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'notes/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def index(request):
+    notes = Note.objects.filter(user=request.user)
+    return render(request, 'notes/index.html', {'notes': notes})
+
